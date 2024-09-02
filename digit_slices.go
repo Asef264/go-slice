802bd4f227781c4e534f12890dee957164b5e268
Asef264 <xhmoazedi@gmail.com>
2024-09-02 12:19:38 +0330

    erase reverse ints

--- digit_slices.go
+++ digit_slices.go
@@ -15,48 +15,6 @@ func NewIntSlice(in []int) IntSlice {
 	return in
 }
 
-// ReverseInteger reverses the elements in an array of integers
-func ReverseInteger(in []int) []int {
-	// Create an output array with the same length as the input array
-	output := make([]int, len(in))
-	for i := 0; i < len(in); i++ {
-		// Reverse the order of elements
-		output[i] = in[len(in)-1-i]
-	}
-	return output
-}
-
-// ReverseInt32 reverses the elements in an array of int32 numbers
-func ReverseInt32(in []int32) []int32 {
-	// Create an output array with the same length as the input array
-	output := make([]int32, len(in))
-	for i := 0; i < len(in); i++ {
-		// Reverse the order of elements
-		output[i] = in[len(in)-1-i]
-	}
-	return output
-}
-
-// ReverseFloat64 reverses the elements in an array of float64 numbers
-func ReverseFloat64(in []float64) []float64 {
-	// Create an output array with the same length as the input array
-	output := make([]float64, len(in))
-	for i := 0; i < len(in); i++ {
-		// Reverse the order of elements
-		output[i] = in[len(in)-1-i]
-	}
-	return output
-}
-
-// ReverseFloat32 reverses the elements in an array of float32 numbers
-func ReverseFloat32(in []float32) []float32 {
-	output := make([]float32, len(in))
-	for i := 0; i < len(in); i++ {
-		output[i] = in[len(in)-1-i]
-	}
-	return output
-}
-
 // SumInt gets a slice in type int and returns a summerize of all elements in int64
 func (is IntSlice) SumInt() int64 {
 	out := ToInt64(is)
